323e607ed151cf01c5181e7ae879f0eb502c32c8
Leandro Caetano de Faria <lcf2212@gmail.com>
2020-01-09 21:58:34 -0300

    aula 04

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from "@angular/core";
-import { SigninComponent } from "./signin/signin.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { VmessageModule } from "../shared/components/vmessage/vmessage.module";
 import { RouterModule } from "@angular/router";
 
+import { SigninComponent } from "./signin/signin.component";
+import { VmessageModule } from "../shared/components/vmessage/vmessage.module";
+import { SignupComponent } from "./signup/signup.component";
+
 @NgModule({
-  declarations: [SigninComponent],
-  imports: [CommonModule, ReactiveFormsModule, VmessageModule, RouterModule]
+  declarations: [SigninComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    VmessageModule,
+    RouterModule
+  ]
 })
 export class HomeModule {}
