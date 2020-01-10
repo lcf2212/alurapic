31479c38d29e9c475528d38b5f879d3461672e0e
lcf2212 <59319081+lcf2212@users.noreply.github.com>
2020-01-10 11:11:46 -0300

    Revert "Feature/parte2 aula5"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app.routing.module";
-import { PhotosModule } from "./photos/photos.module";
-import { ErrorsModule } from "./errors/errors.module";
-import { HomeModule } from "./home/home.module";
-import { CoreModule } from "./core/core.module";
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    BrowserModule,
-    PhotosModule,
-    ErrorsModule,
-    CoreModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
